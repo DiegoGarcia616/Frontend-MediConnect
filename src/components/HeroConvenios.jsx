e6353cd968f5d1f6ae36eb308aee717587e82c72
Diego García <u23247615@utp.edu.pe>
2026-05-27 19:55:32 -0500

    Implementación de Convenios con components

---
+++ src/components/HeroConvenios.jsx
@@ -0,0 +1,45 @@
+import { Container, Row, Col, Image } from "react-bootstrap";
+import imagenConvenios from "../images/imagenconvenios.png";
+
+export default function HeroConvenios() {
+    return (
+        <Container className="my-5">
+            <Row className="align-items-center">
+                <Col md={6}>
+                    <h1
+                        className="fw-bold mb-4 text-start"
+                        style={{ fontSize: "3rem" }}
+                    >
+                        Nuestros Convenios
+                    </h1>
+
+                    <p
+                        className="text-start"
+                        style={{
+                            fontSize: "1.3rem",
+                            lineHeight: "1.8",
+                        }}
+                    >
+                        Accede a una amplia red de convenios con seguros,
+                        autoseguro y aseguradoras internacionales para
+                        recibir atención médica de calidad.
+                    </p>
+                </Col>
+
+                {/* Esto es para la imagen de la sección convenioss */}
+                <Col md={6}>
+                    <Image
+                        src={imagenConvenios} alt="Convenios Clínica"
+                        fluid
+                        style={{
+                            width: "100%",
+                            borderRadius: "30px",
+                            objectFit: "cover",
+                        }}
+                    />
+                </Col>
+
+            </Row>
+        </Container>
+    );
+}
--- src/pages/public/Convenios.jsx
+++ src/pages/public/Convenios.jsx
@@ -1,3 +1,57 @@
+import HeroConvenios from "../../components/HeroConvenios";
+import SeccionLista from "../../components/SeccionLista";
+
 export default function Convenios() {
-    return <div>Convenios</div>
-  }
+
+  const seguros = [
+    "RIMAC Seguros",
+    "Sanitas",
+    "Interseguro SOAT",
+    "Protecta Security",
+    "Pacífico Seguros",
+    "La Positiva Seguros",
+    "MAPFRE",
+    "Crecer Seguros",
+    "Vivir Seguros",
+  ];
+
+  const autoseguros = [
+    "Autoseguros",
+    "Sedapal - Minicetur - Sima",
+    "Cobertura Médica",
+    "Fondo de Empleados del BCRP",
+    "Fesalud",
+  ];
+
+  const internacionales = [
+    "United Healthcare",
+    "Cigna",
+    "Prestige International",
+    "VUMI",
+    "Best Doctors Insurance",
+    "Redbridge Insurance Company, Ltd.",
+    "Allianz Worldwide Care",
+    "Henner",
+  ];
+
+  return (
+    <>
+      <HeroConvenios />
+
+      <SeccionLista
+        titulo="Compañías de Seguros"
+        items={seguros}
+      />
+
+      <SeccionLista
+        titulo="Autoseguros y Convenios Especiales"
+        items={autoseguros}
+      />
+
+      <SeccionLista
+        titulo="Internacionales"
+        items={internacionales}
+      />
+    </>
+  );
+}
